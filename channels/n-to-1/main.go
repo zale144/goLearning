0be9830b18554ff912c98b73bae0a06bcc4b5b32
Aleksandar Šuković <aleksandar.sukovic@nth.ch>
2017-06-22 17:30:12 +0200

    n-to-1 wait group

--- channels/n-to-1/main.go
+++
@@ -1,1 +0,0 @@
-package n_to_1
--- channels/range-clause/main.go
+++ channels/range-clause/main.go
@@ -1,1 +1,18 @@
-package range_clause
+package main
+
+import "fmt"
+
+func main() {
+	c := make(chan int)
+
+	func() {
+		for i := 0; i < 10; i++ {
+			c <- i
+		}
+		close(c)
+	}()
+
+	for n:= range c {	//range blocks it
+		fmt.Println(n)
+	}
+}
