a1570f47849bb9d66dad2e134533cc86328397b2
Aleksandar Šuković <aleksandar.sukovic@nth.ch>
2017-06-22 15:46:37 +0200

    new lessons, concurrency, parallelism

---
+++ race-condition/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"sync"
+	"fmt"
+	"time"
+	"math/rand"
+)
+
+var wg sync.WaitGroup
+var counter int
+
+func main() {
+	wg.Add(2)
+	go incrementor("Foo:")
+	go incrementor("Bar:")
+	wg.Wait()
+	fmt.Println("Final Counter: ", counter)
+}
+
+func incrementor(s string) {
+	for i := 0; i < 20; i++ {
+		x := counter
+		x++
+		time.Sleep(time.Duration(rand.Intn(3)) * time.Millisecond)
+		counter = x
+		fmt.Println(s, i, "Counter: ", counter)
+	}
+	wg.Done()
+}
